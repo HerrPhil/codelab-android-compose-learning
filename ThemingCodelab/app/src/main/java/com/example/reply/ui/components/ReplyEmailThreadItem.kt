820fe52d484e28bf533ab0e3ca7884e2058249d0
herrphil <phillip.j.wray.esq@gmail.com>
2025-11-24 16:51:27 -0700

    Completed the theming codelab

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyEmailThreadItem.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyEmailThreadItem.kt
@@ -16,6 +16,7 @@
 
 package com.example.reply.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -44,10 +45,20 @@ fun ReplyEmailThreadItem(
     email: Email,
     modifier: Modifier = Modifier
 ) {
+
+    // Lesson: the position of the .background MATTERS. I ran the app with it
+    // (1) before padding 16
+    // (2) between padding 16 and padding 20
+    // (3) after padding 20
+    // the amount of screen showing the background color changes.
     Column(
         modifier = modifier
             .fillMaxWidth()
             .padding(16.dp)
+            .background(
+                color = MaterialTheme.colorScheme.background,
+                shape = MaterialTheme.shapes.medium
+            )
             .padding(20.dp)
     ) {
         Row(modifier = Modifier.fillMaxWidth()) {
@@ -63,9 +74,11 @@ fun ReplyEmailThreadItem(
             ) {
                 Text(
                     text = email.sender.firstName,
+                    style = MaterialTheme.typography.labelMedium
                 )
                 Text(
                     text = stringResource(id = R.string.twenty_mins_ago),
+                    style = MaterialTheme.typography.labelMedium
                 )
             }
             IconButton(
@@ -83,11 +96,14 @@ fun ReplyEmailThreadItem(
 
         Text(
             text = email.subject,
+            style = MaterialTheme.typography.bodyMedium,
             modifier = Modifier.padding(top = 12.dp, bottom = 8.dp),
         )
 
         Text(
             text = email.body,
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
         )
         Row(
             modifier = Modifier
